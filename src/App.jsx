74e5338645bc1278f83c4c93e92652ddb04cdca3
Tad Miller <chrchpre@gmail.com>
2025-03-13 22:58:48 -0400

    TM: add const basename

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import LoginForm from './components/LoginForm';
 import About from './components/About';
 import './styles.css';
 
+const basename = process.env.NODE_ENV === 'production' ? '/bible-chronology' : '/';
 
 function App() {
   return ( 
     <StrictMode>
-        <BrowserRouter basename="/bible-chronology">
+        <BrowserRouter basename={basename}>
           <Routes>
           <Route path="/" element={<Landing />} />
             <Route path="/login" element={<LoginForm />} />
