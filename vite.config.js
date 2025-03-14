21bae0faa1646a91fe3505a862e596fd3a2102f9
Tad Miller <chrchpre@gmail.com>
2025-03-13 22:40:43 -0400

    TM: try base again

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bible-chronology/',
   build: {
     outDir: 'dist',                    // Output folder
     assetsDir: 'assets',               // Subdirectory for static assets
